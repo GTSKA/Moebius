03d261fb4c167af75a33cfb223331de55d97319c
Gene Trigo <gene.trigo@gameloft.com>
2018-07-18 12:25:28 -0700

    +Adding index Buffer

--- VulkanTest/VulkanTest/Main.cpp
+++ VulkanTest/VulkanTest/Main.cpp
@@ -135,6 +135,7 @@ private:
 		createFramebuffers();//Drawing tutorial
 		createCommandPool();//Drawing tutorial
 		createVertexBuffer();
+		createIndexBuffer();
 		createCommandBuffers();//Drawing tutorial
 		createSyncObjects();
 	}
@@ -560,7 +561,9 @@ private:
 			VkBuffer vertexBuffers[] = { vertexBuffer };
 			VkDeviceSize offsets[] = { 0 };
 			vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-			vkCmdDraw(commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+			vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+			vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
+			//vkCmdDraw(commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0, 0);
 			//vertexCount, instanceCount, firstVertex, firstInstance
 			vkCmdEndRenderPass(commandBuffers[i]);
 			
@@ -626,6 +629,25 @@ private:
 
 		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
 		copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+		vkDestroyBuffer(device, stagingBuffer, nullptr);
+		vkFreeMemory(device, stagingBufferMemory, nullptr);
+	}
+
+	void createIndexBuffer()
+	{
+		VkDeviceSize bufferSize = sizeof(indices[0])*indices.size();
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), (size_t)bufferSize);
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+		copyBuffer(stagingBuffer, indexBuffer, bufferSize);
 		vkDestroyBuffer(device, stagingBuffer, nullptr);
 		vkFreeMemory(device, stagingBufferMemory, nullptr);
 	}
@@ -874,6 +896,8 @@ private:
 	void cleanup()
 	{
 		cleanupSwapChain();
+		vkDestroyBuffer(device, indexBuffer, nullptr);
+		vkFreeMemory(device, indexBufferMemory, nullptr);
 		vkDestroyBuffer(device, vertexBuffer, nullptr);
 		vkFreeMemory(device, vertexBufferMemory, nullptr);
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i)
@@ -1096,10 +1120,12 @@ private:
 	const std::vector<const char*> validationLayers = { "VK_LAYER_LUNARG_standard_validation" };
 	const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 	const std::vector<Vertex> vertices = {
-		Vertex(Vector2(0.0f, -0.5f), Vector3(1.0f, 1.0f, 0.0f)),
-		Vertex(Vector2(0.5f, 0.5f), Vector3(0.0f, 1.0f, 1.0f)),
-		Vertex(Vector2(-0.5f, 0.5f), Vector3(1.0f, 0.0f, 1.0f))
+		Vertex(Vector2(-0.5f, -0.5f), Vector3(1.0f, 1.0f, 0.0f)),
+		Vertex(Vector2(0.5f, -0.5f), Vector3(0.0f, 1.0f, 1.0f)),
+		Vertex(Vector2(0.5f, 0.5f), Vector3(1.0f, 0.0f, 1.0f)),
+		Vertex(Vector2(-0.5f, 0.5f), Vector3(1.0f, 1.0f, 1.0f))
 	};
+	const std::vector<uint16_t> indices = { 0,1,2,2,3,0 };
 #ifdef NDEBUG
 	const bool enableValidationLayers = false;
 #else
@@ -1123,8 +1149,11 @@ private:
 	VkQueue graphicsQueue;
 	VkQueue presentQueue;
 	VkCommandPool commandPool;
+
 	VkBuffer vertexBuffer;
 	VkDeviceMemory vertexBufferMemory;
+	VkBuffer indexBuffer;
+	VkDeviceMemory indexBufferMemory;
 	
 	std::vector<VkSemaphore> imageAvailableSemaphore;
 	std::vector<VkSemaphore> renderFinishedSemaphore;
